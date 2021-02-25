9db67ad4862be4d3125f484496c37af6e1161961
Joshua Stamps <stampsmed91@gmail.com>
2021-02-24 18:36:51 -0600

    API controllers for cart and category type

---
+++ Backend/src/main/java/com/ecommerce/shop/api/CategoryController.java
@@ -0,0 +1,44 @@
+package com.ecommerce.shop.api;
+
+import com.ecommerce.shop.entity.ProductCategory;
+import com.ecommerce.shop.entity.ProductInfo;
+import com.ecommerce.shop.service.*;
+import com.ecommerce.shop.validation.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Joshua Stamps on 2/24/2021
+ */
+@RestController
+@CrossOrigin
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductService productService;
+
+    /**
+     * Show products in category
+     *
+     * @param categoryType
+     * @param page
+     * @param size
+     * @return
+     */
+    @GetMapping("/category/{type}")
+    public CategoryPage showOne(@PathVariable("type") Integer categoryType,
+                                @RequestParam(value = "page", defaultValue = "1") Integer page,
+                                @RequestParam(value = "size", defaultValue = "3") Integer size) {
+
+        productCategory cat = categoryService.findByCategoryType(categoryType);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<ProductInfo> productInCategory = productService.findAllInCategory(categoryType, request);
+        var tmp = new CategoryPage("", productInCategory);
+        tmp.setCategory(cat.getCategoryName());
+        return tmp;
+    }
+}
